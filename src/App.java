b9670182699eca55d9aaef7dfa3318e031063b61
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-11-07 16:33:30 +0800

    Reset.

--- src/App.java
+++ src/App.java
@@ -1,12 +1,10 @@
-
 import java.util.Scanner;
 
 public class App {
+  public static void main(String[] args) {
+    // Handler for inputs.
+    Scanner mainOptionSc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        // Handler for inputs.
-        Scanner mainOptionSc = new Scanner(System.in);
-
-        System.out.println("Hello, World!");
-    }
+    System.out.println("Hello, World!");
+  }
 }
--- src/Credit.java
+++
@@ -1,9 +0,0 @@
-
-public class Credit {
-
-    public String accountNumber;
-    public String accountName;
-    public String annualIncome;
-    public String contactNumber;
-
-}
