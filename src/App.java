051812ab50e56c1f986cfbb8ece27cefafcae3f4
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-11-08 22:04:53 +0800

    AUthor.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 /**
- * Author: Kent Louise Tonino
+ * Author: Kent Louise G. Tonino
  * Program: Bachelor of Science, Computer Engineering
  * Student #: 2022-0076767
  * Course: CS6203 - Object Oriented Programming
