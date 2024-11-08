deaf7dbc6186f38219784468e5dcdfcd4ff7690b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-11-08 22:14:33 +0800

    Add color.

--- src/libs/Description.java
+++ src/libs/Description.java
@@ -2,9 +2,12 @@ package src.libs;
 
 public class Description {
 
+    static String blue = "\033[34m";
+    static String reset = "\033[0m";
+
     public static void main() {
-        System.out.println("\t------------------------------------------");
+        System.out.println(blue + "\t------------------------------------------" + reset);
         System.out.println("\t\tCredit Account Application");
-        System.out.println("\t------------------------------------------");
+        System.out.println(blue + "\t------------------------------------------" + reset);
     }
 }
