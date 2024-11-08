2b8cc386cb02b8c1103bd298ac5c1af16b2796b7
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-11-08 22:22:12 +0800

    Add extra spaces.

--- src/libs/Description.java
+++ src/libs/Description.java
@@ -15,11 +15,11 @@ public class Description {
     }
 
     public static void options() {
-        System.out.println("\t[1] New Account");
-        System.out.println("\t[2] Credit Inquiry");
-        System.out.println("\t[3] Purchase");
-        System.out.println("\t[4] Payment");
-        System.out.println("\t[5] Close Account");
-        System.out.println("\t[6] Exit");
+        System.out.println("\t[ 1 ] New Account");
+        System.out.println("\t[ 2 ] Credit Inquiry");
+        System.out.println("\t[ 3 ] Purchase");
+        System.out.println("\t[ 4 ] Payment");
+        System.out.println("\t[ 5 ] Close Account");
+        System.out.println("\t[ 6 ] Exit");
     }
 }
