c26ea24281d0b1f795363ad682b07870887d3d64
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-11-08 22:16:45 +0800

    Make it cool.

--- src/libs/Description.java
+++ src/libs/Description.java
@@ -3,11 +3,14 @@ package src.libs;
 public class Description {
 
     static String blue = "\033[34m";
+    static String green = "\033[32m";
     static String reset = "\033[0m";
 
     public static void main() {
-        System.out.println(blue + "\t------------------------------------------" + reset);
-        System.out.println("\t\tCredit Account Application");
-        System.out.println(blue + "\t------------------------------------------" + reset);
+        System.out.println(blue + "\t----------------------------------------------" + reset);
+        Helper.addNewline();
+        System.out.println(green + "\t\tCredit Account Management App" + reset);
+        Helper.addNewline();
+        System.out.println(blue + "\t----------------------------------------------" + reset);
     }
 }
